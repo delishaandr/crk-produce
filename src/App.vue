4ca9e68ec1a7fe2b685217a19f546c61719a7dab
delishaandr <icha.delishaan@gmail.com>
2024-03-13 20:30:47 +0700

    show product detail on select

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-3">
+  <div class="p-4">
     <div class="row">
       <div class="col-md-4 mb-2">
         <div class="mb-3">
@@ -20,7 +20,6 @@
             label="name" 
             placeholder="Select goods..." 
             :options="options"
-            :reduce="(option) => option.id"
           >
             <template #option="option">
               <div class="d-flex">
@@ -34,14 +33,21 @@
         </div>
       </div>
       <div class="col-md-8">
-        <div>
-          produce name + time etc
+        <div v-if="isSelected" class="d-flex align-items-center">
+          <div class="p-2">
+            <img :src="selectedGoods.image_url" width="50px">
+          </div>
+          <div class="p-2 flex-fill" style="font-size: large;">{{ selectedGoods.name }}</div>
+          <div class="p-2" style="text-align: end;">
+            <div>{{ formatTime(selectedGoods.production_time) }}</div>
+            <div>{{ getBuilding(selectedGoods.building_id) }}</div>
+          </div>
         </div>
-        <div class="row">
-          <div class="col-md-6">
+        <div class="mt-2 row">
+          <div v-if="isSelectedGoods" class="col-md-6">
             ingredients
           </div>
-          <div class="col-md-6">
+          <div v-if="isSelected" class="col-md-6">
             used in
           </div>
         </div>
@@ -52,6 +58,7 @@
 
 <script>
 import products from './data/products.json'
+import product_ingredients from './data/product_has_ingredients.json'
 import buildings from './data/buildings.json'
 import vSelect from 'vue-select'
 import 'vue-select/dist/vue-select.css';
@@ -62,19 +69,53 @@ export default {
     vSelect
   },
   data(){
-    return{
+    return {
       products: products,
       buildings: buildings,
       options: products,
 
       selectedBuilding: '',
-      selectedGoods: ''
+      selectedGoods: null,
+
+      selectedIngredients: [],
+      isSelected: 0,
+      isSelectedGoods: 0
+    }
+  },
+  watch: {
+    selectedBuilding(value) {
+      this.selectedGoods = null
+      if (value !== null) {
+        this.options = this.products.filter((p) => p.building_id === value )
+      } else {
+        this.options = this.products
+      }
+    },
+    selectedGoods(value) {
+      if (value !== null) {
+        this.isSelected = 1
+        this.isSelectedGoods = value.is_goods
+      } else {
+        this.isSelected = 0
+        this.isSelectedGoods = 0
+        this.selectedIngredients = []
+      }
     }
   },
   methods: {
-    onChangeBuilding() {
-      console.log('aaadasda')
-      console.log(this.selectedBuilding)
+    formatTime(timeSec) {
+      let hours = Math.floor(timeSec / 3600)
+      let minutes = Math.floor((timeSec - (hours * 3600)) / 60)
+      let seconds = timeSec - (hours * 3600) - (minutes * 60)
+
+      let text = ''
+      if (hours !== 0) text += `${hours}h `
+      if (minutes !== 0) text += `${minutes}m `
+      if (seconds !== 0) text += `${seconds}s `
+      return text
+    },
+    getBuilding(id) {
+      return this.buildings.find((b) => b.id === id).name
     }
   }
 }
